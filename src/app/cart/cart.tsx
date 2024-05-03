f509a6f6ff1d8a5aa02b1acf5feb67d2cc0bf31b
bryanfernandesFrwk <bryanfernandes@frwk.com.br>
2024-05-03 15:43:06 -0300

    feat: cart

--- src/app/cart/cart.tsx
+++ src/app/cart/cart.tsx
@@ -5,6 +5,13 @@ import ProductCart from "./prouctCart";
 export default function Cart({show, close} : {show: boolean, close: () => void}) {
     const {products} = useCartContext()
 
+    const totalValueInCart = products.reduce((acummulator, nextProd) => acummulator + (nextProd.value * nextProd.amount), 0)
+
+    const totalValueInBRL = new Intl.NumberFormat("pt-BR", {
+      currency: "BRL",
+      style: "currency"
+  }).format(totalValueInCart)
+
     return <div className={`absolute right-0 top-0 h-screen ${show? "w-screen": "w-0"} transition-[width] transition-700 bg-white z-50`}>
     {show && <div className="p-8 flex flex-col relative gap-8">
       <span onClick={close} className="cursor-pointer self-end font-bold">X</span>
@@ -12,6 +19,8 @@ export default function Cart({show, close} : {show: boolean, close: () => void})
       <div className="flex gap-4 flex-wrap justify-center">
         {products.map(prod => <ProductCart key={prod.id} {...prod} />)}
       </div>
+
+      <span className="self-end p-8 font-bold">Total: {totalValueInBRL}</span>
     </div>}
   </div>
 }
--- src/app/cart/prouctCart.tsx
+++ src/app/cart/prouctCart.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { IProductCart, useCartContext } from "../context/cart";
+import limitaTamanhoString from "../helpers/functions/limitaTamanhoString";
 
 export default function ProductCart({image, name, amount, id, value} : IProductCart) {
     const {decreaseProductAmount, increaseProductAmount, removeFromCart} = useCartContext()
@@ -11,10 +12,12 @@ export default function ProductCart({image, name, amount, id, value} : IProductC
         style: "currency"
     }).format(value * amount)
 
-    return <div className="flex gap-8 border-2 border-black rounded pr-3">
+    return <div className="flex flex-col gap-2 border-2 border-black rounded">
+        <span className="self-end font-bold cursor-pointer pr-2" onClick={() => removeFromCart(id)}>X</span>
         <Image {...image} className="w-44 h-52" alt={image.alt} width={176} height={250} />
-        <div className="flex flex-col gap-4 justify-between">
-            <h2 className="uppercase font-bold border-b-2 border-black font-serif">{name}</h2>
+        <div className="flex flex-col gap-4 justify-between p-2">
+            <h2 className="uppercase font-bold border-b-2 border-black font-serif" title={name}>{limitaTamanhoString({string: name, tamanho: 12})}</h2>
+            
             <div className="flex justify-between">
                 <div className="flex gap-2 font-bold">
                     <span className="cursor-pointer" onClick={() => decreaseProductAmount(id)}>-</span>    
@@ -24,6 +27,5 @@ export default function ProductCart({image, name, amount, id, value} : IProductC
                 <h2>{totalValueInBRL}</h2>
             </div>
         </div>
-        <span className="font-bold cursor-pointer mt-3" onClick={() => removeFromCart(id)}>X</span>
     </div>
 }
